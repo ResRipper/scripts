83526c93e2c29a142ea8ad1b90110d66c4c96384
ResRipper <resripper@connective.link>
2025-09-02 13:08:30 +0800

    ignore duplicate filename / file missing

--- media/convert_image_format.py
+++ media/convert_image_format.py
@@ -32,7 +32,7 @@ def image_clean(items: list[str]):
             else:
                 os.remove(item.rsplit('.', 1)[0] + '.webp')
         except FileNotFoundError:
-            print(f'Duplicate file name: {item}')
+            continue
 
 
 def convert(folder: str) -> bool:
