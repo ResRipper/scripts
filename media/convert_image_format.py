a8a5aee114dbe0f76120efef76531bc4e24009ab
ResRipper <resripper@connective.link>
2026-06-21 13:48:05 +0800

    Use libwebp for webp; convert to jxl by-default
    Note: mogrify might produce animated webp with gliches, also will generate larger jxl image compare to cjxl

--- media/convert_image_format.py
+++ media/convert_image_format.py
@@ -1,48 +1,115 @@
 #!/bin/python3
 # Convert images in folders to webp format and only keeping the samller files
-# Prerequisites: imagemagick, libwebp
+# Prerequisites:
+#   - webp: libwebp
+#   - jxl: libjxl, imagemagick (webp), libwebp (webp)
 
-# Make sure to check for warnings and errors in the output!
+# !! Test before use !!
+# !! Make sure to check for warnings and errors in the output !!
 
+import glob
 import os
 import sys
 from argparse import ArgumentParser
+from functools import partial
 from multiprocessing import Pool
 from os import chdir
 from os import listdir
 from os import walk
 from os.path import getsize
+from secrets import token_hex
+from shutil import rmtree
 from subprocess import check_output
 from subprocess import run
 
 
-def __conv_image(file: str):
-    # Webp image size limit: 16384 x 16384 pixels
-    image_size = check_output(['identify', file]).decode().removeprefix(file).strip().split(' ')[1].split('x')
-    for i in image_size:
-        if int(i) > 16384:
-            print(f'Error: skipping {file}, size exceeds limit: {image_size[0]}x{image_size[1]}')
-            return
+def __conv_image(format: str, file: str, parallel: int):
+    """Convert image format.
 
-    # Convert
-    run(['mogrify', '-format', 'webp', '-define', 'webp:lossless=true', '-quality', '100', file])
+    Args:
+        format (str): Output image format
+        file (str): Source image file path
+        parallel (int): Number of worker threads for cjxl
+    """
+    file_name = file.rsplit('.', 1)[0]
+    try:
+        match format:
+            case 'webp':
+                # Webp image size limit: 16384 x 16384 pixels
+                image_size = (
+                    check_output(['identify', file]).decode().removeprefix(file).strip().split(' ')[1].split('x')
+                )
+                for i in image_size:
+                    if int(i) > 16384:
+                        print(f'Error: skipping {file}, size exceeds limit: {image_size[0]}x{image_size[1]}')
+                        return
+                # Convert
+                if file.rsplit('.', 1)[-1] == 'gif':
+                    run(['gif2webp', '-quiet', '-mt', '-min_size', file, '-o', f'{file_name}.webp'])
+                else:
+                    run(['img2webp', '-quiet', '-lossless', '-min_size', file, '-o', f'{file_name}.webp'])
+
+            case 'jxl':
+                # Lossless, max effort, default worker threads
+                command = ['cjxl', '--quiet', '-d', '0', '-e', '10', f'--num_threads={parallel}']
+                match file.rsplit('.', 1)[-1]:
+                    # cjxl doesn't support webp -> jxl natively
+                    case 'webp':
+                        if 'animated' in check_output(['file', file]).decode():
+                            # webp -> *.png -> gif -> jxl
+                            temp_folder = f'{file_name}_{token_hex(3)}'
+                            os.mkdir(temp_folder)
+                            run(['anim_dump', '-folder', temp_folder, file])
+                            run(
+                                [
+                                    'magick',
+                                    '-quiet',
+                                    '-delay',
+                                    '10',
+                                    '-loop',
+                                    '0',
+                                    *glob.glob(f'{temp_folder}/*.png'),
+                                    f'{file_name}.gif',
+                                ]
+                            )
+                            rmtree(temp_folder)
+                            run([*command, f'{file_name}.gif', f'{file_name}.jxl'])
+                            os.remove(f'{file_name}.gif')
+                        else:
+                            # webp -> png -> jxl
+                            tmp_file = f'{file_name}_tmp_{token_hex(3)}.png'
+                            run(['dwebp', '-quiet', file, '-o', tmp_file])
+                            run([*command, tmp_file, f'{file_name}.jxl'])
+                            os.remove(tmp_file)
+                    case 'jpg' | 'jpeg':
+                        run([*command, '--lossless_jpeg=1', file, f'{file_name}.jxl'])
+                    case _:
+                        run([*command, file, f'{file_name}.jxl'])
+
+            case _:
+                print(f'Unknown format for file: {file}')
+                return
+    except Exception:
+        print(f'Convert failed: {file}')
+        return
 
     # Remove larger file
     try:
-        if getsize(file) > getsize(file.rsplit('.', 1)[0] + '.webp'):
+        if getsize(file) > getsize(file_name + f'.{format}'):
             os.remove(file)
         else:
-            os.remove(file.rsplit('.', 1)[0] + '.webp')
+            os.remove(file.rsplit('.', 1)[0] + f'.{format}')
     except FileNotFoundError:
         pass
 
 
-def convert(folder: str, parallel: int = os.process_cpu_count()) -> None:
-    """Convert images to webp format and remove larger files.
+def convert(folder: str, format: str, parallel: int) -> None:
+    """Convert process starter.
 
     Args:
         folder (str): Folder that contain images
-        parallel (int): Number of files to be processed in parallel, default: avaliable CPU core count
+        format (str): Output image format
+        parallel (int): Number of worker threads for cjxl
     """
     print('Current folder: ' + folder)
     items = listdir(folder)
@@ -50,8 +117,17 @@ def convert(folder: str, parallel: int = os.process_cpu_count()) -> None:
     target_items = []
 
     for item in items:
-        if item.endswith(('.jpg', '.jpeg', '.png')):
-            target_items.append(item)
+        match item.rsplit('.', 1)[-1]:
+            case 'jpg' | 'jpeg' | 'png' | 'gif':
+                target_items.append(item)
+            case 'webp':
+                if format != 'webp':
+                    target_items.append(item)
+            case 'jxl':
+                if format != 'jxl':
+                    target_items.append(item)
+            case _:
+                continue
 
     if len(target_items) == 0:
         print('No image found or all images are webp format.')
@@ -61,20 +137,27 @@ def convert(folder: str, parallel: int = os.process_cpu_count()) -> None:
 
     chdir(folder)
 
-    with Pool(processes=parallel) as pool:
-        pool.map(__conv_image, target_items)
-        chdir('../')
+    for item in target_items:
+        __conv_image(format, item, parallel)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument(
-        '-p', '--parallel',
+        '-p',
+        '--parallel',
         type=int,
         default=os.process_cpu_count(),
-        help='Number of files to be processed in parallel (default: Avaliable CPU core count)'
+        help='Number of worker threads for cjxl (default: Avaliable CPU core count)',
+    )
+    parser.add_argument(
+        '-f', '--format', type=str, default='jxl', help='Output format, options: jxl, webp (default: jxl)'
     )
     parser.add_argument('folder', help='Folder to be processed')
     args = parser.parse_args()
+
+    if args.parallel <= 0:
+        raise ValueError('Invalid thread count.')
 
     chdir(args.folder)
     folders: list[str] = next(walk('.'))[1]
@@ -84,6 +167,6 @@ if __name__ == '__main__':
         folders.sort()
 
     for folder in folders:
-        convert(folder, parallel=args.parallel)
+        convert(folder, format=args.format, parallel=args.parallel)
 
     print('Done.')
